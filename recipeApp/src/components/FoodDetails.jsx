9cfcd599801e5de524fa9e7ddaea2ecb380e679d
prabinthapa10 <prabinmagar636@gmail.com>
2024-10-09 16:52:19 +0545

    Fetching instruction

--- recipeApp/src/components/FoodDetails.jsx
+++ recipeApp/src/components/FoodDetails.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import styles from "./FoodDetails.module.css";
 
 import "./FoodDetails.module.css";
 export default function FoodDetails({ foodId }) {
@@ -8,20 +9,46 @@ export default function FoodDetails({ foodId }) {
   // store data got from api'
   const [recipeInfo, setRecipeInfo] = useState({});
 
+  const [isLoading, setLoading] = useState(true);
   useEffect(() => {
     async function fetchFood() {
       const response = await fetch(`${URL}?apiKey=${API_KEY}`);
       const data = await response.json();
       console.log(data);
       setRecipeInfo(data);
+      setLoading(false);
     }
     fetchFood();
   }, [foodId]);
   return (
     <div>
-      <h1>Food Details</h1>
-      {foodId}
-      {recipeInfo.dishTypes}
+      <div>
+        <div>
+          <h1 className={styles.title}>{recipeInfo.title}</h1>
+          <img src={recipeInfo.image} alt="image" />
+          <span>
+            <strong>⌚{recipeInfo.readyInMinutes} Minutes</strong>
+          </span>
+          <span>
+            <strong>👪 Serves {recipeInfo.servings}</strong>
+          </span>
+          <span>{recipeInfo.vegetarian ? "Veg 🍠" : "Non Veg 🐃"}</span>
+          <div>
+            <span>💲{recipeInfo.pricePerServing / 100} Per serving</span>
+          </div>
+
+          <div>
+            <h1>Instruction</h1>
+            {isLoading ? (
+              <p>Loading.</p>
+            ) : (
+              recipeInfo.analyzedInstructions[0].steps.map((step) => (
+                <li>{step.step}</li>
+              ))
+            )}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
